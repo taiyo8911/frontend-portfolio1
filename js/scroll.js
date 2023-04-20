6c2456a3d00a0f5d96d994f63f646f975712eee3
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2023-04-21 08:57:10 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,29 @@
-function disableScroll(event) {
-    event.preventDefault();
-}
+// // スクロールを無効にするための関数
+// function disableScroll(event) {
+//     event.preventDefault();
+// }
 
-// イベントと関数を紐付け
-document.addEventListener('touchmove', disableScroll, { passive: false });
+// // touchmoveイベント時にスクロールを無効にする関数を呼び出すように設定
+// document.addEventListener('touchmove', disableScroll, { passive: false });
+
+
+// // スクロールを無効化
+// document.documentElement.style.overflow = 'hidden';
+// document.body.style.overflow = 'hidden';
+// document.addEventListener('touchmove', scrollControl, { passive: false });
+
+// // スクロールを有効化
+// document.documentElement.style.overflow = '';
+// document.body.style.overflow = '';
+// document.removeEventListener('touchmove', scrollControl, { passive: false });
+
+
+// // .js-can-scroll要素内はスクロールできるようにする
+// var scrollControl = function (event) {
+//     if (event.target.closest('.js-can-scroll')) {
+//         event.stopPropagation();
+//     } else {
+//         event.preventDefault();
+//     }
+// };
+
